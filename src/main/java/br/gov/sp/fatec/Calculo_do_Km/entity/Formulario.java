1416d846dfd1bec3fd4d5c4710abe34c17541802
herculespsilva <herculespsilva@hotmail.com>
2021-08-13 21:27:50 +0000

    fefrfr

--- src/main/java/br/gov/sp/fatec/Calculo_do_Km/entity/Formulario.java
+++ src/main/java/br/gov/sp/fatec/Calculo_do_Km/entity/Formulario.java
@@ -25,124 +25,24 @@ private Long id;
 @Column(name = "for_modelo")
 private String modelo;
 
-private float(5,2) valor_automovel;
-@Column(name = "for_modelo")
+// @Digits(integer=5, fraction=2)
+// @Column(name = "for_valor_automovel")
+// private BigDecimal  valor_automovel;
 
-private float(5,2) depreciacao;
-@Column(name = "for_modelo")
+public Long getId() {
+    return this.id;
+}
 
-private float(5,2) km_mes;
-@Column(name = "for_modelo")
+public void setId(Long id) {
+    this.id = id;
+}
 
-private float(5,2) km_anual;
-@Column(name = "for_modelo")
+public String getModelo() {
+    return this.modelo;
+}
 
-private float(5,2) valor_depreciação;
-@Column(name = "for_modelo")
-
-private float(5,2) ipva;
-@Column(name = "for_modelo")
-
-private float(5,2) preco_seguro_anual;
-@Column(name = "for_modelo")
-
-private float(5,2) custo_seguro_licenciamento_km;
-@Column(name = "for_modelo")
-
-private float(5,2) custo_pneu;
-@Column(name = "for_modelo")
-
-private float(5,2) vida_util_pneu;
-@Column(name = "for_modelo")
-
-private float(5,2) custo_pneu_km;
-@Column(name = "for_modelo")
-
-private float(5,2) custo_troca_oleo;
-@Column(name = "for_modelo")
-
-private float(5,2) frequencia_km_troca;
-@Column(name = "for_modelo")
-
-private float(5,2) custo_oleo_km;
-@Column(name = "for_modelo")
-
-private float(5,2) custo_manutenção_anual;
-@Column(name = "for_modelo")
-
-private float(5,2) custo_manutencao_km;
-@Column(name = "for_modelo")
-
-private float(5,2) aluguel_mensal;
-@Column(name = "for_modelo")
-
-private float(5,2) custo_aluguel_km;
-@Column(name = "for_modelo")
-
-private float(5,2) preco_gasolina;
-@Column(name = "for_modelo")
-
-private float(5,2) consumo_gasolina;
-@Column(name = "for_modelo")
-
-private float(5,2) custo_gasolina_km;
-@Column(name = "for_modelo")
-
-private float(5,2) preco_etanol;
-@Column(name = "for_modelo")
-
-private float(5,2) consumo_etano;
-@Column(name = "for_modelo")
-
-private float(5,2) custo_etanol_km;
-@Column(name = "for_modelo")
-
-private float(5,2) preco_gnv;
-@Column(name = "for_modelo")
-
-private float(5,2) consumo_gnv;
-@Column(name = "for_modelo")
-
-private float(5,2) custo_gnv_km;
-@Column(name = "for_modelo")
-
-private float(5,2) salario_me;
-@Column(name = "for_modelo")
-
-private float(5,2) custo_salario_km;
-@Column(name = "for_modelo")
-
-private float(5,2) custo_total_gasolina_km;
-@Column(name = "for_modelo")
-
-private float(5,2) custo_total_gasolina_mensal_km;
-@Column(name = "for_modelo")
-
-private float(5,2) custo_total_etanol_km;
-@Column(name = "for_modelo")
-
-private float(5,2) custo_total_etanol_mensal_km;
-@Column(name = "for_modelo")
-
-private float(5,2) custo_total_gnv_km;
-@Column(name = "for_modelo")
-
-private float(5,2) custo_total_gnv_mensal_km;
-@Column(name = "for_modelo")
-
-private float(5,2) ganho_medio_corrida_km;
-@Column(name = "for_modelo")
-
-private float(5,2) ganho_total;
-@Column(name = "for_modelo")
-
-private float(5,2) lucro_final_gasolina;
-@Column(name = "for_modelo")
-
-private float(5,2) lucro_final_etanol;
-@Column(name = "for_modelo")
-
-private float(5,2) lucro_final_gnv;
-@Column(name = "for_modelo")
+public void setModelo(String modelo) {
+    this.modelo = modelo;
+}
 
 }
