7938ef763ee14fbcdd8d335659a697467fedac2e
herculespsilva <herculespsilva@hotmail.com>
2021-08-18 23:24:34 +0000

    Ajustes nos repository

--- src/main/java/br/gov/sp/fatec/Calculo_do_Km/repository/FormularioRepository.java
+++ src/main/java/br/gov/sp/fatec/Calculo_do_Km/repository/FormularioRepository.java
@@ -19,12 +19,10 @@ public interface FormularioRepository extends JpaRepository<Formulario, Long> {
     @Query("select f from Formulario f where f.modelo =?1")
     public List<Formulario> buscaFormularioPorModelo(String modelo);
 
-    @Query("select f from Formulario f inner join f.usuario d where d.nome =?1 and f.valor_automovel = ?2")
-    public List<Formulario> buscaFormularioPorModeloEValor(String nome, BigDecimal valor);
+    @Query("select f from Formulario f inner join f.usuario u where u.id =?1 ")
+    public List<Formulario> buscaTodosFormularioPorUsuarioId(String nome);
 
-    // @Query("select f from Filme f inner join f.diretor d where d.nome =?1")
-    // public List<Filme> buscaFilmePorDiretor(String diretor);
-
-    // @Query("select f from Filme f inner join f.diretor d where f.nome = ?1 and d.nome = ?2")
-    // public Formulario buscaFilmePorNomeEDiretor(String filme, String diretor);
+    @Query("select f from Formulario f inner join f.usuario u where u.nome =?1 and f.valor_automovel = ?2")
+    public List<Formulario> buscaFormularioPorUsuarioNomeEValor(String nome, BigDecimal valor);
+    
 }
