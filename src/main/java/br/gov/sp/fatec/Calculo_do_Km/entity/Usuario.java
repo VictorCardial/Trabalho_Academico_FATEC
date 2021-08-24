e56b4020cda17f32cc9b5c284324130c15185db4
VictorCardial <victor_cardial@hotmail.com>
2021-08-24 14:03:48 +0000

    Corrigindo Classes

--- src/main/java/br/gov/sp/fatec/Calculo_do_Km/entity/Usuario.java
+++ src/main/java/br/gov/sp/fatec/Calculo_do_Km/entity/Usuario.java
@@ -46,7 +46,7 @@ public class Usuario {
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario")
     private List<Formulario> formularios;
 
-    /**Marca */
+    /**Marca... */
     /**Getters e Setters dos atributos */
 
     public Long getId() {
