b579e39f4a2dea811015aea1473c21c47df5fa2b
herculespsilva <herculespsilva@hotmail.com>
2021-08-26 02:02:27 +0000

    Alteracao nos testes

--- src/main/java/br/gov/sp/fatec/Calculo_do_Km/repository/FormularioRepository.java
+++ src/main/java/br/gov/sp/fatec/Calculo_do_Km/repository/FormularioRepository.java
@@ -10,19 +10,19 @@ import br.gov.sp.fatec.Calculo_do_Km.entity.Formulario;
 
 public interface FormularioRepository extends JpaRepository<Formulario, Long> {
 
-    @Query("select f from Formulario f where f.modelo =?1")
+    @Query("select f from Formulario f where f.id =?1")
     public Formulario buscaFormularioPorIdQuery(Long id);
 
     @Query("select f from Formulario f")
     public Formulario buscaTodosFormulariosQuery();
 
     @Query("select f from Formulario f where f.modelo =?1")
-    public List<Formulario> buscaFormularioPorModelo(String modelo);
+    public List<Formulario> buscaFormularioPorModeloQuery(String modelo);
 
-    @Query("select f from Formulario f inner join f.usuario u where u.id =?1 ")
-    public List<Formulario> buscaTodosFormularioPorUsuarioId(String nome);
+    @Query("select f from Formulario f inner join f.usuario u where u.nome =?1 ")
+    public List<Formulario> buscaTodosFormularioPorUsuarioNomeQuery(String nome);
 
     @Query("select f from Formulario f inner join f.usuario u where u.nome =?1 and f.valor_automovel = ?2")
-    public List<Formulario> buscaFormularioPorUsuarioNomeEValor(String nome, BigDecimal valor);
+    public List<Formulario> buscaFormularioPorUsuarioNomeEValorQuery(String nome, BigDecimal valor);
     
 }
--- src/test/java/br/gov/sp/fatec/Calculo_do_Km/FormularioTests.java
+++ src/test/java/br/gov/sp/fatec/Calculo_do_Km/FormularioTests.java
@@ -1,13 +1,105 @@
 package br.gov.sp.fatec.Calculo_do_Km;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import br.gov.sp.fatec.Calculo_do_Km.entity.Formulario;
+import br.gov.sp.fatec.Calculo_do_Km.entity.Usuario;
+import br.gov.sp.fatec.Calculo_do_Km.repository.FormularioRepository;
+import br.gov.sp.fatec.Calculo_do_Km.repository.UsuarioRepository;
 
 @SpringBootTest
+@Transactional
+@Rollback
 class FormularioTests {
+
+	@Autowired
+	FormularioRepository formRepo;
+
+	@Autowired
+	UsuarioRepository usuarioRepo; 
+
 
 	@Test
 	void contextLoads() {
+	}
+
+	// save
+	@Test
+    void testaCriacaoFormulario() {
+		Usuario usuario = new Usuario();
+		usuario.setNome("Lucas");
+		usuario.setSenha("123");
+		usuarioRepo.save(usuario);
+
+		Formulario form = new Formulario();
+		form.setUsuario(usuario);
+		form.setModelo("renault kwid");
+		form.setValorAutomovel(new BigDecimal("40000"));
+		formRepo.save(form);
+
+		assertEquals("renault kwid", form.getModelo());
+		assertNotNull(form.getId());
+	}	
+	
+	// delete
+	// @Test
+    // void testaDeleteFormulario() {
+	// 	Formulario formulario = formRepo.buscaFormularioPorIdQuery(1l);
+	// 	formRepo.delete(formulario);
+
+	// 	Formulario auxForm = formRepo.buscaFormularioPorIdQuery(1l);
+	// }
+
+	// update
+	@Test
+	void testaAtualizacaoFormulario() {
+		Usuario usuario = new Usuario();
+		usuario.setNome("Joao");
+		usuario.setSenha("123");
+		usuarioRepo.save(usuario);
+
+		Formulario form = new Formulario();
+		form.setUsuario(usuario);
+		form.setModelo("Honda Civic");
+		form.setValorAutomovel(new BigDecimal("90000"));
+		formRepo.save(form);
+
+		form.setModelo("Fiat Palio");
+
+		assertEquals("Fiat Palio", form.getModelo());
+	}
+
+	// restrições (exceptions)
+	
+
+	// teste repository
+	@Test
+	void testaBuscaFormularioPorModeloQuery() {
+		List<Formulario> formulario = formRepo.buscaFormularioPorModeloQuery("Corolla");
+		assertNotNull(formulario);
+	}
+
+	@Test
+	void testaBuscaTodosFormularioPorUsuarioNomeQuery() {
+		List<Formulario> formulario = formRepo.buscaTodosFormularioPorUsuarioNomeQuery("victor");
+		assertNotNull(formulario);
+	}
+
+	// relacionamentos
+	@Test
+	void tesaBuscaFormularioPorUsuarioNomeEValorQuery() {
+		List<Formulario> formulario = formRepo.buscaFormularioPorUsuarioNomeEValorQuery("hercules", new BigDecimal("5001"));
+		assertTrue(formulario.isEmpty());
 	}
 
 }
