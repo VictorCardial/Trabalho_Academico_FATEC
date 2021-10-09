467f703d35fbf43a5848e5fefc05a5f5dc7580f6
herculespsilva <herculespsilva@hotmail.com>
2021-10-09 22:27:48 +0000

    implementção de jwt authentication

--- src/main/java/br/gov/sp/fatec/Calculo_do_Km/security/SecurityConfig.java
+++ src/main/java/br/gov/sp/fatec/Calculo_do_Km/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package br.gov.sp.fatec.Calculo_do_Km.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +12,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -21,7 +23,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().httpBasic().and() /* desabilita metodo de securança por token, qunado ferenciado pelo proprio spring (gerando as paginas html)*/
+        http.csrf().disable() /* desabilita metodo de securança por token, qunado ferenciado pelo proprio spring (gerando as paginas html)*/
+        .addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
         .sessionManagement()
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS); // entre uma requição e outra não utilizar a mesma area de memoria (sem reaproveitar cache)
     }
@@ -34,5 +37,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean /*disponibliza para o spring, usar o @Autowired (usar em outros lugares) devido ser um clase gerenciado pelo proprio Spring*/
     public PasswordEncoder passwordEncoderBean() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+      return super.authenticationManagerBean();
     }
 }
